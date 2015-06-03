deef872fa0987ff9b916501a24f3fca26bc11e78
eythimis <eythimis@gmail.com>
2015-06-03 10:16:28 +0300

    stable - ready to add refresh tokens

---
+++ PairPark/Utils/Startup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+using Newtonsoft.Json.Serialization;
+using Microsoft.Owin.Security.OAuth;
+using System.Web.Http.Tracing;
+
+[assembly: OwinStartup(typeof(PairPark.Utils.Startup))]
+
+namespace PairPark.Utils
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //configure oauth for owin
+            ConfigureOAuth(app);
+
+            //configure owin to play with web api
+            ConfigureWebApi(app);
+        }
+
+        private void ConfigureWebApi(IAppBuilder app)
+        {
+            var configuration = new HttpConfiguration();
+
+            //default route
+            //configuration.Routes.MapHttpRoute(
+            //    "default",
+            //    "api/{controller}"
+            //    );
+
+            //to be able to set Routes on top of action, controller, etc...
+            configuration.MapHttpAttributeRoutes();
+
+            //format json output
+            var jsonFormatter = configuration.Formatters.JsonFormatter;
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            
+            //use cors *
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            //enable tracing
+            SystemDiagnosticsTraceWriter traceWriter = configuration.EnableSystemDiagnosticsTracing();
+            traceWriter.IsVerbose = true;
+            traceWriter.MinimumLevel = TraceLevel.Off;
+
+            app.UseWebApi(configuration);
+        }
+
+        private void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new AuthorizationServerProvider()              
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+        }
+    }
+}
